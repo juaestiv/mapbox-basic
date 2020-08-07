bc9c1af2e4759c8f6b3e568957961c2b022284a4
juanan <juaestiv@gmail.com>
2020-08-07 19:48:36 +0200

    initial without confijg.json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import mapboxgl from 'mapbox-gl';
+import config from './config';
+import "mapbox-gl/dist/mapbox-gl.css";
+
+const styles = {
+  width: "100vw",
+  height: "calc(100vh - 80px)",
+  position: "absolute"
+};
 
 function App() {
+  const [map, setMap] = useState(null);
+  const mapContainer = useRef(null);
+
+  useEffect(() => {
+    mapboxgl.accessToken = config.token;
+    const initializeMap = ({ setMap, mapContainer }) => {
+      const map = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+        center: [-104.9876, 39.7405],
+        zoom: 5
+      });
+
+      map.on("load", () => {
+        setMap(map);
+        map.resize();
+      });
+    };
+
+    if (!map) initializeMap({ setMap, mapContainer });
+  }, [map]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div ref={el => (mapContainer.current = el)} style={styles} />
   );
 }
 
